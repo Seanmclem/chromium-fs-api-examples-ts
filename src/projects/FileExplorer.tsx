662708f125949b0f57420d2ee6ee55101d9704cf
Seanmclem <seanmclements@gmail.com>
2020-12-18 16:14:37 -0500

    added basic initial file menu

--- src/projects/FileExplorer.tsx
+++ src/projects/FileExplorer.tsx
@@ -2,14 +2,21 @@ import React, { useCallback, useState } from "react"
 import { DirectoryContents } from "../components/directoryContents/DirectoryContents"
 import styled from "styled-components"
 import { FileViewerByType } from "../components/FileViewerByType"
-
+import { FileMenu } from "../components/FileMenu"
 
 const FileExplorerContainer = styled.div`
+    height: 100vh;
+`
+
+const InnerStuffContainer = styled.div`
     display: flex;
     flex-direction: row;
+    height: 95%;
 `
 
 export const FileExplorer: React.FC<any> = () => {
+    const [altRootHandle, setAltRootHandle] = useState<FileSystemDirectoryHandle| undefined>(undefined)
+
     const [selectedFile, setSelectedFile] = useState<FileSystemHandle| undefined>(undefined)
 
     const handleSelectFileCustom = useCallback(async (file: FileSystemFileHandle) => {
@@ -19,11 +26,15 @@ export const FileExplorer: React.FC<any> = () => {
 
     return (
         <FileExplorerContainer>
-            <DirectoryContents //needs memoized when sober
-                handleSelectFile={handleSelectFileCustom}
-            /> 
-            {/* move condition into component */}
-            {selectedFile && <FileViewerByType fileHandle={selectedFile as FileSystemFileHandle} />}
+            <FileMenu setAltRootHandle={setAltRootHandle} />
+            <InnerStuffContainer>
+                <DirectoryContents //needs memoized when sober
+                    handleSelectFile={handleSelectFileCustom}
+                    altRootHandle={altRootHandle}
+                /> 
+                {/* move condition into component */}
+                {selectedFile && <FileViewerByType fileHandle={selectedFile as FileSystemFileHandle} />}
+            </InnerStuffContainer>
         </FileExplorerContainer>
     )
 }
---
+++ src/types/index.d.ts
@@ -0,0 +1,1 @@
+declare module '@szhsin/react-menu';
