98958d49cab10dd1e43f98d5519c5d5afed208bc
Seanmclem <seanmclements@gmail.com>
2021-02-27 17:24:08 -0500

    start building out form component

--- src/pages/FormCreator.tsx
+++ src/pages/FormCreator.tsx
@@ -1,10 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
+import { TextInput } from "ready-fields"
 
-// const handleFormSubmit = () => {
+const FormCreatorContainer = styled.div`
+    margin: 20px;
+    label {
+        margin-right: 20px;
+    }
+`
 
-// }
+const Preview = styled.div`
+    margin: 10px;
+    border: 1px solid black;
+    max-width: 500px;
+    padding: 20px;
+`
+
+const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+}
 
 export const FormCreator: React.FC<{}> = () => {
     // Start with a basic working form
-    return <div>form creator</div>
+    const [firstName, setFirstName] = useState<string>("")
+    return (
+        <FormCreatorContainer>
+            <form onSubmit={handleSubmit}>
+                <TextInput
+                    name="first-name"
+                    label="First Name"
+                    text={firstName}
+                    setText={setFirstName}
+                />
+            </form>
+            <Preview>
+                firstName: {firstName}
+            </Preview>
+        </FormCreatorContainer>
+    )
 }
