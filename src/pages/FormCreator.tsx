07e6cbd786086ae8fb5d2d0a799115fb076e1306
Seanmclem <seanmclements@gmail.com>
2021-02-27 17:37:45 -0500

    moar prepping form-creator

--- src/pages/FormCreator.tsx
+++ src/pages/FormCreator.tsx
@@ -2,30 +2,45 @@ import React, { useState } from "react"
 import styled from "styled-components"
 import { TextInput } from "ready-fields"
 
-const FormCreatorContainer = styled.div`
-    margin: 20px;
-    label {
-        margin-right: 20px;
-    }
-`
+// const FormCreatorContainer = styled.div`
+//     margin: 20px;
+//     label {
+//         margin-right: 20px;
+//     }
+// `
 
-const Preview = styled.div`
-    margin: 10px;
-    border: 1px solid black;
-    max-width: 500px;
-    padding: 20px;
-`
+// const Preview = styled.div`
+//     margin: 10px;
+//     border: 1px solid black;
+//     max-width: 500px;
+//     padding: 20px;
+// `
+
+{/* <Preview>
+    <p>
+        firstName: {firstName}
+    </p>
+    <p>
+        phone: {phone}
+    </p>
+    <p>
+        email: {email}
+    </p>
+</Preview> */}
+
 
 const handleSubmit = (event: any) => {
     event.preventDefault();
-
+    debugger;
 }
 
 export const FormCreator: React.FC<{}> = () => {
     // Start with a basic working form
     const [firstName, setFirstName] = useState<string>("")
+    const [phone, setPhone] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
     return (
-        <FormCreatorContainer>
+        <div>
             <form onSubmit={handleSubmit}>
                 <TextInput
                     name="first-name"
@@ -33,10 +48,26 @@ export const FormCreator: React.FC<{}> = () => {
                     text={firstName}
                     setText={setFirstName}
                 />
+                <TextInput
+                    name="phone"
+                    label="Phone"
+                    text={phone}
+                    setText={setPhone}
+                    type="tel"
+                />
+                <TextInput
+                    name="email"
+                    label="Email"
+                    text={email}
+                    setText={setEmail}
+                    type="email"
+                />
+                <button
+                    type="submit"
+                >
+                    Submit
+                </button>
             </form>
-            <Preview>
-                firstName: {firstName}
-            </Preview>
-        </FormCreatorContainer>
+        </div>
     )
 }
