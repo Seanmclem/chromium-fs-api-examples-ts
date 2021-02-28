7d0cf58913a8b847af1365bdf3ab95dcd241d2db
Seanmclem <seanmclements@gmail.com>
2021-02-27 20:20:35 -0500

    form creator mini

--- src/pages/FormCreator.tsx
+++ src/pages/FormCreator.tsx
@@ -1,33 +1,5 @@
 import React, { useState } from "react"
-import styled from "styled-components"
 import { TextInput } from "ready-fields"
-
-// const FormCreatorContainer = styled.div`
-//     margin: 20px;
-//     label {
-//         margin-right: 20px;
-//     }
-// `
-
-// const Preview = styled.div`
-//     margin: 10px;
-//     border: 1px solid black;
-//     max-width: 500px;
-//     padding: 20px;
-// `
-
-{/* <Preview>
-    <p>
-        firstName: {firstName}
-    </p>
-    <p>
-        phone: {phone}
-    </p>
-    <p>
-        email: {email}
-    </p>
-</Preview> */}
-
 
 const handleSubmit = (event: any) => {
     event.preventDefault();
@@ -35,7 +7,6 @@ const handleSubmit = (event: any) => {
 }
 
 export const FormCreator: React.FC<{}> = () => {
-    // Start with a basic working form
     const [firstName, setFirstName] = useState<string>("")
     const [phone, setPhone] = useState<string>("")
     const [email, setEmail] = useState<string>("")
