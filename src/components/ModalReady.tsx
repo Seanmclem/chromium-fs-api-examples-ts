41da4eaefc79f372021c94f2bf5aa3b19607a3a5
Seanmclem <seanmclements@gmail.com>
2021-03-21 22:13:26 -0400

    fit that

--- src/components/ModalReady.tsx
+++ src/components/ModalReady.tsx
@@ -20,7 +20,7 @@ const ModalBody = styled.div`
   margin: 15% auto; /* 15% from the top and centered */
   padding: 20px;
   border: 1px solid #888;
-  width: 80%; /* Could be more or less, depending on screen size */
+  width: fit-content;
 `
 interface props {
     children: any,
